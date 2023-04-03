df4a00993df07727bb16f1a15a061027e806e43f
linjh <516308181@qq.com>
2023-04-03 14:22:34 +0800

    03-对参数进行验证

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { TransformInterceptor } from './core/interceptor/transform/transform.interceptor';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   // const app = await NestFactory.create(AppModule);
@@ -17,6 +18,7 @@ async function bootstrap() {
   SwaggerModule.setup('docs', app, document);
   // app.setGlobalPrefix('api'); // 设置全局路由前缀
   app.useGlobalInterceptors(new TransformInterceptor());  // 拦截
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(7899);
 }
 bootstrap();
--- src/posts/dto/create-post.dot.ts
+++ src/posts/dto/create-post.dot.ts
@@ -1,12 +1,17 @@
 // dto/create-post.dot.ts
 import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 export class CreatePostDto {
   @ApiProperty({ description: '文章标题' })
+  @IsNotEmpty({ message: '文章标题必填' })
   readonly title: string;
+  @IsNotEmpty({ message: '缺少作者信息' })
   @ApiProperty({ description: '作者' })
   readonly author: string;
+  @IsString()
   @ApiPropertyOptional({ description: '内容' })
   readonly content: string;
+  // @IsString()
   @ApiPropertyOptional({ description: '文章封面' })
   readonly cover_url: string;
   @ApiProperty({ description: '文章类型' })
